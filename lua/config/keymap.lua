761d9ad0a7844aefe8d2e60a6fc8d1ee0b13ebc8
Chris Nicholas <cn@onthelawn.xyz>
2023-10-17 19:10:20 -0500

    Add keymap to switch windows

--- lua/config/keymap.lua
+++ lua/config/keymap.lua
@@ -1,6 +1,12 @@
 local utils = require('utils.keymap')
 local nmap = utils.nmap
 
+-- Select window with <option> + h,j,k,l
+nmap("<A-k>", "<C-w>k")
+nmap("<A-j>", "<C-w>j")
+nmap("<A-h>", "<C-w>h")
+nmap("<A-l>", "<C-w>l")
+
 -- Scroll with arrow keys
 nmap("<Up>", "<c-y>")
 nmap("<Down>", "<c-e>")

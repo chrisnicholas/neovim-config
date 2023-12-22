50922ff010a9fb0dfea85ad285db11e1d371e2a3
Chris Nicholas <cn@onthelawn.xyz>
2023-12-22 15:01:50 -0600

    Fix ruby textobjects... sort of

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -1,22 +1,12 @@
 local M = {
-    'nvim-treesitter/nvim-treesitter',
-    lazy = false,
-    build = ':TSUpdate',
-    event = { "BufReadPost", "BufNewFile", "BufWritePre" },
-  }
+  'nvim-treesitter/nvim-treesitter',
+  lazy = false,
+  build = ':TSUpdate',
+  event = { "BufReadPost", "BufNewFile", "BufWritePre" },
+}
 
 M.opts = {
-  highlight = {
-    enable = true,
-    disable = function(_, buf)
-      local max_filesize = 100 * 1024 -- 100 KB
-      local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-      if ok and stats and stats.size > max_filesize then
-        return true
-      end
-    end,
-    additional_vim_regex_highlighting = false,
-  },
+  highlight = { enable = true },
   autopairs = { enable = true },
   autotag = { enable = true },
   indent = { enable = true },
@@ -31,6 +21,7 @@ M.opts = {
     "json",
     "lua",
     "markdown",
+    "python",
     "query",
     "ruby",
     "rust",
@@ -49,6 +40,59 @@ M.opts = {
     },
     highlight_current_scope = { enable = false },
   },
+  incremental_selection = {
+    enable = true,
+    keymaps = {
+      init_selection = '<c-space>',
+      node_incremental = '<c-space>',
+      scope_incremental = '<c-s>',
+      node_decremental = '<M-space>',
+    },
+  },
+  textobjects = {
+    select = {
+      enable = true,
+      lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
+      keymaps = {
+        -- You can use the capture groups defined in textobjects.scm
+        ['aa'] = '@parameter.outer',
+        ['ia'] = '@parameter.inner',
+        ['af'] = '@function.outer',
+        ['if'] = '@function.inner',
+        ['ac'] = '@class.outer',
+        ['ic'] = '@class.inner',
+      },
+    },
+    move = {
+      enable = true,
+      set_jumps = true, -- whether to set jumps in the jumplist
+      goto_next_start = {
+        [']m'] = '@function.outer',
+        [']]'] = '@class.outer',
+      },
+      goto_next_end = {
+        [']M'] = '@function.outer',
+        [']['] = '@class.outer',
+      },
+      goto_previous_start = {
+        ['[m'] = '@function.outer',
+        ['[['] = '@class.outer',
+      },
+      goto_previous_end = {
+        ['[M'] = '@function.outer',
+        ['[]'] = '@class.outer',
+      },
+    },
+    swap = {
+      enable = true,
+      swap_next = {
+        ['<leader>a'] = '@parameter.inner',
+      },
+      swap_previous = {
+        ['<leader>A'] = '@parameter.inner',
+      },
+    },
+  },
 }
 
 function M.config(_, opts)
@@ -56,11 +100,11 @@ function M.config(_, opts)
   nvim_treesitter.setup(opts)
 
   -- Code Folding
-  vim.opt.foldmethod = "expr"
-  vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
-  vim.opt.foldlevel = 99
-  vim.opt.foldlevelstart = -1
-  vim.opt.foldenable = true
+  -- vim.opt.foldmethod = "expr"
+  -- vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+  -- vim.opt.foldlevel = 99
+  -- vim.opt.foldlevelstart = -1
+  -- vim.opt.foldenable = true
 end
 
 return M
--- lua/plugins/vim-rails.lua
+++ lua/plugins/vim-rails.lua
@@ -1,6 +1,8 @@
 local M = {
   'tpope/vim-rails',
+  lazy = true,
   ft = 'ruby',
+  enabled = false
 }
 
 return M

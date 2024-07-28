dc03000c91aa095e17b350a58fd9c60d621650d7
Chris Nicholas <cn@onthelawn.xyz>
2024-07-27 22:56:08 -0500

    Use non-deprecated function

--- lua/plugins/auto-dark-mode.lua
+++ lua/plugins/auto-dark-mode.lua
@@ -2,13 +2,13 @@ return {
   'f-person/auto-dark-mode.nvim',
   lazy = false,
   opts = {
-    update_interval = 1000,
+    update_interval = 500,
     set_dark_mode = function()
-      vim.api.nvim_set_option('background', 'dark')
+      vim.api.nvim_set_option_value('background', 'dark', {})
       vim.cmd.colorscheme('github_dark_default')
     end,
     set_light_mode = function()
-      vim.api.nvim_set_option('background', 'light')
+      vim.api.nvim_set_option_value('background', 'light', {})
       vim.cmd.colorscheme('github_light_default')
     end,
   },

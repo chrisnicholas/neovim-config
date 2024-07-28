41a5920cc99eceb433f145e82eff2c4447cac63f
Chris Nicholas <cn@onthelawn.xyz>
2024-07-27 23:33:55 -0500

    Consolidate all colorscheme related configs

--- lua/plugins/colorschemes/init.lua
+++ lua/plugins/colorschemes/init.lua
@@ -1,3 +1,6 @@
+local LIGHT_COLORSCHEME = 'github_light_default'
+local DARK_COLORSCHEME = 'github_dark_default'
+
 local M = {}
 
 function M.config()
@@ -5,8 +8,34 @@ function M.config()
     require('plugins.colorschemes.catppuccin'),
     require('plugins.colorschemes.github-nvim-theme'),
     require('plugins.colorschemes.oxocarbon'),
-    require('plugins.colorschemes.tokyonight')
+    require('plugins.colorschemes.tokyonight'),
+    {
+      'f-person/auto-dark-mode.nvim',
+      lazy = false,
+      opts = M.opts()
+    }
   }
+end
+
+function M.opts()
+  return {
+    update_interval = 500,
+    set_dark_mode = M.set_dark_mode,
+    set_light_mode = M.set_light_mode
+  }
+end
+
+function M.set_light_mode()
+  M.set_colorscheme(LIGHT_COLORSCHEME, 'light', {})
+end
+
+function M.set_dark_mode()
+  M.set_colorscheme(DARK_COLORSCHEME, 'dark', {})
+end
+
+function M.set_colorscheme(colorscheme, variant, opts)
+  vim.api.nvim_set_option_value('background', variant, opts)
+  vim.cmd.colorscheme(colorscheme)
 end
 
 return M.config()

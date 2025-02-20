ce003a3f392ce3c419fe81dc89b014e23b732064
Chris Nicholas <cn@onthelawn.xyz>
2025-02-20 09:15:03 -0600

    Update nvim-cmp bindings

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -35,8 +35,8 @@ function M.config(_, opts)
       ['<C-f>'] = cmp.mapping.scroll_docs(4),
       ['<C-Space>'] = cmp.mapping.complete(),
       ['<C-e>'] = cmp.mapping.abort(),
-      ['<CR>'] = cmp.mapping.confirm({ select = true }),
-      ['<Tab>'] = cmp.mapping(function(fallback)
+      ['<C-CR>'] = cmp.mapping.confirm({ select = true }),
+      ['<C-n>'] = cmp.mapping(function(fallback)
         if cmp.visible() then
           cmp.select_next_item()
         elseif luasnip.expand_or_jumpable() then
@@ -48,7 +48,7 @@ function M.config(_, opts)
         end
       end, { 'i', 's' }),
 
-      ['<S-Tab>'] = cmp.mapping(function(fallback)
+      ['<C-p>'] = cmp.mapping(function(fallback)
         if cmp.visible() then
           cmp.select_prev_item()
         elseif luasnip.jumpable(-1) then

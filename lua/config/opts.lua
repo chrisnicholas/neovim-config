1903e5b0a5b123168598f7e812a51f4d7733ba9e
Chris Nicholas <cn@onthelawn.xyz>
2025-02-26 15:01:02 -0600

    Change minimum line number width

--- lua/config/opts.lua
+++ lua/config/opts.lua
@@ -1,7 +1,16 @@
--- Options
+-- Line Numbers
 vim.opt.number = true
+vim.opt.numberwidth = 8
+vim.opt.signcolumn = 'yes'
+
+-- Mouse
 vim.opt.mouse = 'a'
+
+-- Word wrap
 vim.opt.wrap = false
+
 vim.opt.completeopt = 'menu'
+
+-- Color Column(s)
 vim.opt.colorcolumn = '120'
 

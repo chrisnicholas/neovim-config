65352c8c3b7cb1634c4ad2629ac56a0851abba3f
Chris Nicholas <cn@onthelawn.xyz>
2025-02-27 10:42:02 -0600

    Update numberwidth. Disable remote plugin providers

--- lua/config/opts.lua
+++ lua/config/opts.lua
@@ -1,6 +1,6 @@
 -- Line Numbers
 vim.opt.number = true
-vim.opt.numberwidth = 8
+vim.opt.numberwidth = 5
 vim.opt.signcolumn = 'yes'
 
 -- Mouse
@@ -14,3 +14,9 @@ vim.opt.completeopt = 'menu'
 -- Color Column(s)
 vim.opt.colorcolumn = '120'
 
+-- Disable remote plugin providers
+vim.g.loaded_python_provider = 0
+vim.g.loaded_python3_provider = 0
+vim.g.loaded_ruby_provider = 0
+vim.g.loaded_perl_provider = 0
+vim.g.loaded_node_provider = 0

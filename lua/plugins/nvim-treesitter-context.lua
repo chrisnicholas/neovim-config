e0d6b6928735ecf162830bd320aa280a93d6891b
Chris Nicholas <cn@onthelawn.xyz>
2025-03-03 12:59:01 -0600

    Refactor dependency configs for copilot and treesitter

--- lua/plugins/nvim-treesitter-context.lua
+++
@@ -1,6 +0,0 @@
-local M = {
-  'nvim-treesitter/nvim-treesitter-context',
-  lazy = true,
-}
-
-return M
--- lua/plugins/nvim-treesitter-refactor.lua
+++
@@ -1,6 +0,0 @@
-local M = {
-  'nvim-treesitter/nvim-treesitter-refactor',
-  lazy = true,
-}
-
-return M

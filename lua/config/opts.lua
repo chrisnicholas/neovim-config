6849c84217502d71d2727db8b53eb25bd728e83e
Chris Nicholas <cn@onthelawn.xyz>
2025-04-14 08:48:40 -0500

    Change titlestring logic

--- lua/config/opts.lua
+++ lua/config/opts.lua
@@ -1,3 +1,25 @@
+local function set_titlestring()
+  local titlestring = ""
+  local program_name = vim.v.progname
+  local current_dir = vim.fn.fnamemodify(vim.fn.getcwd(), ':~:.:t')
+  local current_file = vim.fn.expand('%:~:.')
+  local current_tab = vim.fn.tabpagenr()
+  local total_tabs = vim.fn.tabpagenr('$')
+  local file_props = "%h%m%r%w" -- h: titlestring
+
+  if vim.fn.expand('%:p') == '' then
+    titlestring = string.format("[%s] %s", program_name, current_dir)
+  else
+    titlestring = string.format("[%s] %s -- %s %s", program_name, current_dir, current_file, file_props)
+  end
+
+  if total_tabs > 1 then
+    titlestring = string.format("%s (Tab %d of %d)", titlestring, current_tab, total_tabs)
+  end
+
+  return titlestring
+end
+
 -- Line Numbers
 vim.opt.number = true
 vim.opt.numberwidth = 5
@@ -5,7 +27,11 @@ vim.opt.signcolumn = 'yes'
 
 -- Let Neovim set the terminal title
 vim.opt.title = true
-vim.opt.titlestring = [[%f %h%m%r%w %{v:progname} (%{tabpagenr()} of %{tabpagenr('$')})]]
+vim.api.nvim_create_autocmd({'BufEnter', 'DirChanged'}, {
+  callback = function()
+    vim.opt.titlestring = set_titlestring()
+  end
+})
 
 -- Mouse
 vim.opt.mouse = 'a'

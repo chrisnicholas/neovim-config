d5febface7786347062b757af5b8d09983a8d199
Chris Nicholas <cn@onthelawn.xyz>
2025-07-02 12:24:35 -0500

    Update nvim-treesitter config

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -1,7 +1,8 @@
 local M = {
-  'nvim-treesitter/nvim-treesitter',
+  "nvim-treesitter/nvim-treesitter",
   lazy = true,
-  build = ':TSUpdate',
+  build = ":TSUpdate",
+  main = "nvim-treesitter.configs",
   event = { "BufReadPost", "BufNewFile", "BufWritePre" },
   dependencies = {
     "nvim-treesitter/nvim-treesitter-textobjects",
@@ -10,100 +11,99 @@ local M = {
   },
 }
 
-M.opts = {
-  highlight = { enable = true },
-  autopairs = { enable = true },
-  autotag = { enable = true },
-  indent = { enable = true },
-  ensure_installed = {
-    "bash",
-    "c",
-    "css",
-    "dockerfile",
-    "go",
-    "html",
-    "javascript",
-    "json",
-    "lua",
-    "markdown",
-    "markdown_inline",
-    "python",
-    "query",
-    "ruby",
-    "rust",
-    "swift",
-    "terraform",
-    "typescript",
-    "vim",
-    "vimdoc",
-  },
-  sync_install = true,
-  ignore_install = {},
-  refactor = {
-    highlight_definitions = {
-      enable = true,
-      clear_on_cursor_move = true,
+function M.opts(_, opts)
+  opts = vim.tbl_deep_extend("force", opts or {}, {
+    highlight = { enable = true },
+    autopairs = { enable = true },
+    autotag = { enable = true },
+    indent = { enable = true },
+    ensure_installed = {
+      "bash",
+      "c",
+      "css",
+      "dockerfile",
+      "go",
+      "html",
+      "javascript",
+      "json",
+      "lua",
+      "markdown",
+      "markdown_inline",
+      "python",
+      "query",
+      "ruby",
+      "rust",
+      "swift",
+      "terraform",
+      "typescript",
+      "vim",
+      "vimdoc",
     },
-    highlight_current_scope = { enable = false },
-  },
-  incremental_selection = {
-    enable = true,
-    keymaps = {
-      init_selection = '<c-space>',
-      node_incremental = '<c-space>',
-      scope_incremental = '<c-s>',
-      node_decremental = '<M-space>',
+    sync_install = true,
+    ignore_install = {},
+    refactor = {
+      highlight_definitions = {
+        enable = true,
+        clear_on_cursor_move = true,
+      },
+      highlight_current_scope = { enable = false },
     },
-  },
-  textobjects = {
-    select = {
+    incremental_selection = {
       enable = true,
-      lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
       keymaps = {
-        -- You can use the capture groups defined in textobjects.scm
-        ['aa'] = '@parameter.outer',
-        ['ia'] = '@parameter.inner',
-        ['af'] = '@function.outer',
-        ['if'] = '@function.inner',
-        ['ac'] = '@class.outer',
-        ['ic'] = '@class.inner',
+        init_selection = "<c-space>",
+        node_incremental = "<c-space>",
+        scope_incremental = "<c-s>",
+        node_decremental = "<M-space>",
       },
     },
-    move = {
-      enable = true,
-      set_jumps = true, -- whether to set jumps in the jumplist
-      goto_next_start = {
-        [']m'] = '@function.outer',
-        [']]'] = '@class.outer',
+    textobjects = {
+      select = {
+        enable = true,
+        lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
+        keymaps = {
+          -- You can use the capture groups defined in textobjects.scm
+          ["aa"] = "@parameter.outer",
+          ["ia"] = "@parameter.inner",
+          ["af"] = "@function.outer",
+          ["if"] = "@function.inner",
+          ["ac"] = "@class.outer",
+          ["ic"] = "@class.inner",
+        },
       },
-      goto_next_end = {
-        [']M'] = '@function.outer',
-        [']['] = '@class.outer',
+      move = {
+        enable = true,
+        set_jumps = true, -- whether to set jumps in the jumplist
+        goto_next_start = {
+          ["]m"] = "@function.outer",
+          ["]]"] = "@class.outer",
+        },
+        goto_next_end = {
+          ["]M"] = "@function.outer",
+          ["]["] = "@class.outer",
+        },
+        goto_previous_start = {
+          ["[m"] = "@function.outer",
+          ["[["] = "@class.outer",
+        },
+        goto_previous_end = {
+          ["[M"] = "@function.outer",
+          ["[]"] = "@class.outer",
+        },
       },
-      goto_previous_start = {
-        ['[m'] = '@function.outer',
-        ['[['] = '@class.outer',
-      },
-      goto_previous_end = {
-        ['[M'] = '@function.outer',
-        ['[]'] = '@class.outer',
+      swap = {
+        enable = true,
+        swap_next = {
+          ["<leader>a"] = "@parameter.inner",
+        },
+        swap_previous = {
+          ["<leader>A"] = "@parameter.inner",
+        },
       },
     },
-    swap = {
-      enable = true,
-      swap_next = {
-        ['<leader>a'] = '@parameter.inner',
-      },
-      swap_previous = {
-        ['<leader>A'] = '@parameter.inner',
-      },
-    },
-  },
-}
+  })
 
-function M.config(_, opts)
-  local nvim_treesitter = require('nvim-treesitter.configs')
-  nvim_treesitter.setup(opts)
+  require("treesitter-context").setup({ enable = true })
 
   -- Code Folding
   vim.opt.foldmethod = "expr"

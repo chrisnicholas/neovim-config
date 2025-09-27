1238ceb1c82888c80113f01c9f3a592ced2d31b7
Chris Nicholas <cn@onthelawn.xyz>
2025-09-26 23:29:41 -0500

    Add VISUAL mode support to copy file path function

--- lua/config/keymap.lua
+++ lua/config/keymap.lua
@@ -21,17 +21,37 @@ nmap("<S-Right>", "zL")
 
 -- Copying file paths to system clipboard
 local function copy_file_path_to_clip_board(expansion)
-  vim.fn.setreg("+", vim.fn.expand(expansion))
+  local file_path = vim.fn.expand(expansion)
+
+  -- Check if we're in visual mode and add line numbers
+  local mode = vim.fn.mode()
+  local visual_block = vim.api.nvim_replace_termcodes('<C-v>', true, true, true)
+  if mode == "v" or mode == "V" or mode == visual_block then -- visual, visual-line, visual-block
+    -- Get visual selection start and end positions
+    local start_pos = vim.fn.getpos("v")
+    local end_pos = vim.fn.getpos(".")
+
+    local start_line = math.min(start_pos[2], end_pos[2])
+    local end_line = math.max(start_pos[2], end_pos[2])
+
+    if start_line == end_line then
+      file_path = file_path .. ":" .. start_line
+    else
+      file_path = file_path .. ":" .. start_line .. "-" .. end_line
+    end
+  end
+
+  vim.fn.setreg("+", file_path)
 end
 
 local function copy_absolute_path_to_clipboard()
   copy_file_path_to_clip_board("%:p")
 end
 
-nmap("<leader>cfp", copy_absolute_path_to_clipboard)
+vim.keymap.set({ "n", "v" }, "<leader>cfp", copy_absolute_path_to_clipboard)
 
 local function copy_relative_path_to_clipboard()
   copy_file_path_to_clip_board("%:.")
 end
 
-nmap("<leader>crp", copy_relative_path_to_clipboard)
+vim.keymap.set({ "n", "v" }, "<leader>crp", copy_relative_path_to_clipboard)
